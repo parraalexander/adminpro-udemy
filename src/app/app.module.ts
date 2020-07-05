7e58ff8ec1d5e64802037c19b2a551738e3f58fa
Alexander <alexander@MacBook-Pro-de-Alexander-2.local>
2020-07-04 21:55:28 -0400

    primero commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { ProgessComponent } from './pages/progess/progess.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { PagesComponent } from './pages/pages.component';
+import { RegisterComponent } from './login/register/register.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NopagefoundComponent,
+    DashboardComponent,
+    Graficas1Component,
+    ProgessComponent,
+    HeaderComponent,
+    BreadcrumbsComponent,
+    SidebarComponent,
+    PagesComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/progess/progess.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-progess',
+  templateUrl: './progess.component.html',
+  styles: [
+  ]
+})
+export class ProgessComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
